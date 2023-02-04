5a5ec98b8667c3867b6341f7e719d812771ef035
YoungPal <jygus513@gmail.com>
2023-02-04 17:06:30 +0900

    correct compilation&logic error

--- orderOfjoomong.cpp
+++ orderOfjoomong.cpp
@@ -9,24 +9,23 @@ int main() {
 
 	int N, M;
 	int temp = 0;
-	itn count = 0;
-	int i=0, j=0
-	vector vector<int> A(N, 0);
+	int count = 0;
+	int i = 0, j = 0;
 
 	cin >> N >> M;
-	
+	vector<int> A(N, 0);
+
 	for (i; i < N; i++) {
 		cin >> A[i];
 	}
 	i = 0;
 
 	for (i; i < M / 2; i++) {
-		for (j; j < N; j++) {
+		for (j=i+1; j < N; j++) {
 			temp = M - A[i];
-			if ((A[j] == M) && i != j) count++;
+			if ((A[j] == temp) && i != j) count++;
 		}
-		j++;
 	}
-	cout << M
+	cout << count;
 
 }
